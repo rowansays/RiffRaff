6f319e928578365d188d59ae04370170d9d8a61e
rowansays <74547960+rowansays@users.noreply.github.com>
2021-02-01 04:00:59 -0800

    Rebuild search functionality. Fixes #4   * Rewrite all js in declaritive Preact and JSX.   * Introduce an aria-live region for better accessibility.   * Replace search results popup with a change-content-in-place strategy.   * Remove javascript twig file.   * Change "fqsen" to "path" in searchIndex.json.   * The former "sidebar" is now known as "menu".   * The former "menu" is slated to be renamed.

--- src/Scripts/search.js
+++ src/Scripts/search.js
@@ -1,193 +1,5 @@
-import Fuse from 'fuse.js'
-
-/**
- * Search module for phpDocumentor
- *
- * This module is a wrapper around fuse.js that will use a given index and
- * attach itself to a search form and to a search results pane identified by
- * the following data attributes:
- *
- * 1. data-search-form
- * 2. #SearchResults
- *
- * The data-search-form is expected to have a single input element of type
- * 'search' that will trigger searching for a series of results, where the
- * #SearchForm pane is expected to have a direct UL child that will be
- * populated with rendered results.
- *
- * The search has various stages, upon loading this stage the data-search-form
- * receives the CSS class 'phpdocumentor-search--enabled'; this indicates that
- * JS is allowed and indices are being loaded. It is recommended to hide the
- * form by default and show it when it receives this class to achieve
- * progressive enhancement for this feature.
- *
- * After loading this module, it is expected to load a search index
- * asynchronously, for example:
- *
- *         <script defer src="js/searchIndex.js"></script>
- *
- * In this script the generated index should attach itself to the search module
- * using the `appendIndex` function. By doing it like this the page will
- * continue loading, unhindered by the loading of the search.
- *
- * After the page has fully loaded, and all these deferred indexes loaded, the
- * initialization of the search module will be called and the form will receive
- * the class 'phpdocumentor-search--active', indicating search is ready. At
- * this point, the input field will also have it's 'disabled' attribute removed.
- */
-var Search = (function () {
-  var fuse;
-  var index = [];
-  var options = {
-    shouldSort: true,
-    threshold: 0.6,
-    location: 0,
-    distance: 100,
-    maxPatternLength: 32,
-    minMatchCharLength: 1,
-    keys: [
-      'fqsen',
-      'name',
-      'summary',
-      'url'
-    ]
-  };
-
-  // Credit David Walsh (https://davidwalsh.name/javascript-debounce-function)
-  // Returns a function, that, as long as it continues to be invoked, will not
-  // be triggered. The function will be called after it stops being called for
-  // N milliseconds. If `immediate` is passed, trigger the function on the
-  // leading edge, instead of the trailing.
-  function debounce(func, wait, immediate) {
-    var timeout;
-
-    return function executedFunction() {
-      var context = this;
-      var args = arguments;
-
-      var later = function () {
-        timeout = null;
-        if (!immediate) {
-          func.apply(context, args);
-        }
-      };
-
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) {
-        func.apply(context, args);
-      }
-    };
-  }
-  function close() {
-    // Start scroll prevention
-    // https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
-    const scrollY = document.body.style.top;
-    document.body.classList.remove('PreventScroll')
-    document.body.style.top = '';
-    window.scrollTo(0, parseInt(scrollY || '0') * -1);
-    // End scroll prevention
-
-    var form = document.querySelector('[data-search-form]');
-    var searchResults = document.getElementById('SearchResults');
-
-    form.classList.toggle('phpdocumentor-search--has-results', false);
-    searchResults.classList.add('Hidden');
-    var searchField = document.querySelector('[data-search-form] input[type="search"]');
-    searchField.blur();
-  }
-
-  function search(event) {
-    // Start scroll prevention
-    // https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
-    document.body.classList.add('PreventScroll')
-    document.body.style.top = `-${window.scrollY}px`;
-    // End scroll prevention
-
-    // prevent enter's from autosubmitting
-    event.stopPropagation();
-
-    var form = document.querySelector('[data-search-form]');
-    var searchResults = document.getElementById('SearchResults')
-    var SearchResultList = document.querySelector('#SearchResultList');
-
-    SearchResultList.innerHTML = '';
-
-    if (!event.target.value) {
-      close();
-      return;
-    }
-
-    form.classList.toggle('phpdocumentor-search--has-results', true);
-    searchResults.classList.remove('Hidden');
-    var results = fuse.search(event.target.value, {limit: 25});
-
-    results.forEach(function (result) {
-      const item = result.item
-      const entry = document.createElement("li");
-      entry.classList.add("SearchResult");
-      entry.innerHTML += '<h3 class="SearchResultName"><a href="' + document.baseURI + item.url + '">' + item.name + "</h3>";
-      entry.innerHTML += '<span class="SearchResultPath">' + item.fqsen + "</span>";
-      entry.innerHTML += '<div class="SearchResultDesc">' + item.summary + '</div>';
-      SearchResultList.appendChild(entry)
-    });
-  }
-
-  function appendIndex(added) {
-    index = index.concat(added);
-
-    // re-initialize search engine when appending an index after initialisation
-    if (typeof fuse !== 'undefined') {
-      fuse = new Fuse(index, options);
-    }
-  }
-
-  function init() {
-    fuse = new Fuse(index, options);
-
-    var form = document.querySelector('[data-search-form]');
-    var searchField = document.querySelector('[data-search-form] input[type="search"]');
-
-    var closeButton = document.getElementById('CloseSearchDialog');
-    closeButton.addEventListener('click', function() { close() }.bind(this));
-
-    var searchResults = document.getElementById('SearchResults')
-    searchResults.addEventListener('click', function() { close() }.bind(this));
-
-    form.classList.add('phpdocumentor-search--active');
-
-    searchField.removeAttribute('disabled');
-    searchField.addEventListener('keyup', debounce(search, 300));
-
-    window.addEventListener('keyup', function (event) {
-      if (event.key === '/') {
-        searchField.focus();
-      }
-      if (event.code === 'Escape') {
-        close();
-      }
-    }.bind(this));
-  }
-
-  return {
-    appendIndex,
-    init
-  }
-})();
-
-window.addEventListener('DOMContentLoaded', function () {
-  var form = document.querySelector('[data-search-form]');
-
-  // When JS is supported; show search box. Must be before including the search
-  // for it to take effect immediately
-  form.classList.add('phpdocumentor-search--enabled');
-});
-
-window.addEventListener('load', function () {
-  Search.init();
-  loadSearchIndex(document.baseURI + '/js/searchIndex.json', Search);
-});
+import { useState } from 'preact/hooks'
+import parse from 'html-react-parser';
 
 /**
  * Load the search index from local json file.
@@ -195,14 +7,207 @@ window.addEventListener('load', function () {
  * @param {string} url Absolute URL to the search index json file.
  * @param {object} Search An instance of the search object.
  *
- * @return {void}
+ * @return {Promise<Array>}
+ * @throws {Error}
  */
-async function loadSearchIndex (url, Search) {
+async function fetchSearchIndex (url) {
   try {
     const indexPromise = await window.fetch(url)
-    const indexValues = await indexPromise.json();
-    Search.appendIndex(indexValues);
+    if (indexPromise.status >= 400) {
+      throw new Error(
+        'Unable to load index from url "' + url + '". ' +
+        'HTTP status code: ' + indexPromise.status
+      )
+    }
+
+    const index = await indexPromise.json();
+    if(!Array.isArray(index)) {
+      throw new Error('Data returned by "' + url + '" must be an array.')
+    }
+
+    if (index.length < 1) {
+      throw new Error('No entries exist in "' + url + '".')
+    }
+
+    return index
   } catch(e) {
-    console.log('Unable to load search index.' + e.message)
+    throw new Error('Search index not loaded. ' + e.message)
   }
 }
+async function fetchMenuItems (url, Search) {
+  try {
+    const fetchPromise = await window.fetch(url)
+    if (fetchPromise.status >= 400) {
+      throw new Error(
+        'Unable to load menu from url "' + url + '". ' +
+        'HTTP status code: ' + fetchPromise.status
+      )
+    }
+
+    const html = await fetchPromise.text();
+    const parsed = parse(html, {
+      library: require('preact'),
+      trim: true
+    })
+
+    if (!parsed || !parsed.props || !parsed.props.id || parsed.props.id !== 'MenuItems') {
+      throw new Error(
+        'Unable to parse menu. The menu at "' + url + '" must contain only a ' +
+        'single element with an id of "MenuItems".'
+      )
+    }
+
+    return parsed
+  } catch(e) {
+    throw new Error('Menu items not loaded. ' + e.message)
+  }
+}
+function Navigation (props) {
+  const { seeker, menuItems } = props
+  const [notice, setNotice] = useState('')
+  const [results, setResults] = useState([])
+  const [searchPhrase, setSearchPhrase] = useState('')
+
+  function q(singular, plural, qty) {
+    return (qty === 1) ? qty + ' ' + singular : qty + ' ' + plural
+  }
+
+  function handleSearchSubmit (event) {
+    event.preventDefault()
+    try {
+      if (searchPhrase.length > 0) {
+        const response = seeker.search(searchPhrase, {limit: 25});
+        const message = q('result was', 'results were', response.length) +
+          ' found when searching for ' + searchPhrase
+        ;
+        setNotice(message)
+        setResults(response)
+      } else {
+        setResults([])
+        setNotice('')
+      }
+    } catch (e) {
+      throw new Error('CAUGHT IT: ' + e.message)
+    }
+  }
+  function handlePhraseChange (event) {
+    const newPhrase = event.target.value
+    if (newPhrase === '') {
+      if (results.length < 1) {
+        setNotice('Phrase deleted from search field.')
+      } else {
+        setNotice('Search results cleared from screen.')
+        setResults([])
+      }
+    }
+    setSearchPhrase(newPhrase)
+  }
+
+  return (
+    <div id="Navigation">
+      <SearchForm
+        phrase={searchPhrase}
+        onSubmit={handleSearchSubmit}
+        onPhraseChange={handlePhraseChange}
+      />
+      <SearchDialog>{notice}</SearchDialog>
+      <div id="Items">
+        <SearchResults results={results} />
+        {menuItems}
+      </div>
+    </div>
+  )
+}
+function SearchForm (props) {
+  const { onSubmit, onPhraseChange, phrase } = props
+  return (
+    <>
+      <form id="Search" method="get" action="" onSubmit={onSubmit}>
+        <label for="SearchPhrase" class="ScreenReaderOnly">Search</label>
+        <input
+          id="SearchPhrase"
+          type="search"
+          placeholder="Search"
+          value={phrase}
+          onInput={onPhraseChange}
+        />
+        <input type="submit" id="SubmitSearch" className="ScreenReaderOnly" />
+        <label for="SubmitSearch">
+          <svg title="Submit Search" class="LookingGlassIcon" width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="7.5" cy="7.5" r="6.5" stroke="currentColor" stroke-width="2"/>
+            <line x1="12.4892" y1="12.2727" x2="19.1559" y2="18.9393" stroke="currentColor" stroke-width="3"/>
+          </svg>
+        </label>
+      </form>
+    </>
+  )
+}
+function SearchDialog (props) {
+  const { children, isOnScreen } = props
+  const atts = {
+    id: 'SearchDialog',
+    'aria-live': 'polite',
+    'aria-atomic': true
+  }
+  if (isOnScreen === false) {
+    atts.className = 'VisuallyHidden'
+  }
+  return <p {...atts}>{children}</p>
+}
+function SearchResults (props) {
+  const { results } = props
+  const atts = {
+    id: 'SearchResults'
+  }
+
+  if (results.length === 0) {
+    atts['aria-hidden'] = true
+    atts.hidden = true
+  }
+
+  const items = results.map(r => {
+    return <SearchResult urlBase={'BANANA'} item={r.item} />
+  })
+
+  return (<ul {...atts}>{items}</ul>)
+}
+function SearchResult (props) {
+  const { item, urlBase } = props
+  const { path, name, summary, url } = item
+  const description = summary === name ? '' : summary
+  const absUrl = urlBase + url !== url ? urlBase + url : ''
+  return (
+    <li class="SearchResult">
+      <SearchResultName url={absUrl}>{name}</SearchResultName>
+      <SearchResultPath>{path}</SearchResultPath>
+      <SearchResultDesc>{description}</SearchResultDesc>
+    </li>
+  )
+}
+function SearchResultDesc (props) {
+  const { children } = props
+  return children !== '' ? <div class="SearchResultDesc">{children}</div> : null
+}
+function SearchResultName (props) {
+  const { children, url } = props
+
+  let content
+  if (url === '' && children === '') {
+    content = null
+  } else if (url === '') {
+    content = (<h3 class="SearchResultName">{children}</h3>)
+  } else {
+    const name = children === '' ? url : children
+    content = (<a href={url}>{name}</a>)
+  }
+
+  return content === null ? content : (
+    <h3 class="SearchResultName">{content}</h3>
+  )
+}
+function SearchResultPath (props) {
+  const { children } = props
+  return children !== '' ? <span class="SearchResultPath">{children}</span> : null
+}
+
+export { fetchSearchIndex, fetchMenuItems, Navigation }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
+import Fuse from 'fuse.js'
 import Prism from 'prismjs';
-import './Scripts/index.js'
+import { render } from 'preact'
+import {
+  fetchSearchIndex,
+  fetchMenuItems,
+  Navigation
+} from './Scripts/search.js'
 import './Styles/index.scss'
 
 Prism.highlightAll();
 
-if (module.hot) {
-  module.hot.accept('./Styles/index.scss', () => {
-    console.log('Accepting style changes.');
-  })
-  module.hot.accept('./Scripts/index.js', () => {
-    console.log('Accepting script changes.');
-  })
-}
+(async () => {
+  try {
+    const menu = document.getElementById('Menu')
+    //const index = await fetchSearchIndex(document.baseURI.split('?')[0] + 'js/searchIndexEmpty.json');
+    //const index = await fetchSearchIndex(document.baseURI.split('?')[0] + 'js/searchIndex.json123');
+    const index = await fetchSearchIndex(document.baseURI.split('?')[0] + 'js/searchIndex.json');
+    const menuItems = await fetchMenuItems(document.baseURI.split('?')[0] + 'js/menu.txt');
+    const fuse = new Fuse(index, {
+      shouldSort: true,
+      threshold: 0.6,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: [ 'path', 'name', 'summary', 'url' ]
+    });
+    render(<Navigation seeker={fuse} menuItems={menuItems} />, menu)
+  } catch (e) {
+    console.error('Search not loaded. ' + e.message)
+  }
+})();
